d67609ec1bbfc4f8d57b7b81aea1da1a06cbcf27
suman1112 <sumandasincalcutta@gmail.com>
2016-09-30 20:33:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
 app.get('/ui/Smiley-thumbs-up-clipart-clipart.jpeg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
